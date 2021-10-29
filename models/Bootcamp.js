1c8c1c14aa6c9d3863cb82c14029b657b054b71e
Amiyanshu-web <59080551+Amiyanshu-web@users.noreply.github.com>
2021-10-29 19:00:46 +0530

    feat: mapquest to get location from address

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
 const slugify=require('slugify');
+const geocoder=require('../utils/geocoder');
 const BootcampSchema=new mongoose.Schema({
     name:{
         type:String,
@@ -48,8 +49,6 @@ const BootcampSchema=new mongoose.Schema({
       index:'2dSphere'
     },
     formattedAddress:String,
-    street:String,
-    city:String,
     street:String,
     city:String,
     state:String,
@@ -101,10 +100,26 @@ const BootcampSchema=new mongoose.Schema({
     }
 
 });
-
+//Slug from anme
 BootcampSchema.pre('save',function(next){
     this.slug=slugify(this.name,{lower:true});
     next();
-})
-
+});
+//Geocode And create location
+    BootcampSchema.pre('save',async function(next){
+        const loc=await geocoder.geocode(this.address);
+        this.location={
+            type:'Point',
+            coordinates:[loc[0].longitude,loc[0].latitude],
+            formattedAddress:loc[0].formattedAddress,
+            street:loc[0].streetName,
+            city:loc[0].city,
+            state:loc[0].stateCode,
+            zipcode:loc[0].zipcode,
+            country:loc[0].countryCode
+        }
+        //dont send address in DB
+        this.address=undefined;
+        next();
+});
 module.exports=mongoose.model('Bootcamp',BootcampSchema);
